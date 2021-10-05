5ef4d4a011b40682977bdc13c70c745ccc06feb0
Victor Edet <edetvictor849@gmail.com>
2021-10-05 08:48:16 +0100

    (ZB) chore: nav bar init

---
+++ src/components/Nav-bottom.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nav-bottom container d-flex mx-auto">
+        <div class="nav-bottom-item col">
+            home
+        </div>
+        <div class="nav-bottom-item col">
+            home
+        </div>
+        <div class="nav-bottom-item col">
+            home
+        </div>
+        <div class="nav-bottom-item col">
+            home
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name:'Nav-bottom'
+    }
+</script>
